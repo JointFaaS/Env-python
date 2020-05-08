eaeb7b44d133c1dfcbdcdf276e5a7a4f3e1dc24a
Veiasai <o206@qq.com>
2020-05-08 23:17:43 +0800

    grpc and tester

--- src/main.py
+++ src/main.py
@@ -4,24 +4,74 @@ import sys
 import zipfile
 import grpc
 import logging
+import requests
+import threading
+import tempfile
+import importlib.util
+from importlib import reload
+
 from concurrent import futures
 
 from container import container_pb2
 from container import container_pb2_grpc
 
 class ContainerSever(container_pb2_grpc.ContainerServicer):
+    def __init__(self):
+        super().__init__()
+        self.loadCodeLock = threading.Lock()
+        os.environ.setdefault('FUNC_NAME', '')
+        self.funcName = os.environ['FUNC_NAME']
+        self.func = None
+        self.d = None
+
     def Invoke(self, request, context: grpc.RpcContext):
-        return container_pb2.InvokeResponse(code=0, )
+        try:
+            if self.func is None:
+                return container_pb2.InvokeResponse(code=1)
+            elif self.funcName != request.funcName:
+                return container_pb2.InvokeResponse(code=2)
+
+            output = self.func.handler(request.payload)
+            return container_pb2.InvokeResponse(code=0, output=output)
+        except Exception as e:
+            logging.warn(e)
+            return container_pb2.InvokeResponse(code=3)
     
     def SetEnvs(self, request, context):
-        pass
+        try:
+            for env in request.env:
+                envs = env.split('=')
+                if len(envs) != 2:
+                    return container_pb2.SetEnvsResponse(code=1)
+                os.environ[envs[0]] = envs[1]
+        except Exception as e:
+            logging.warn(e)
+            return container_pb2.SetEnvsResponse(code=1)
+        return container_pb2.SetEnvsResponse(code=0)
 
     def LoadCode(self, request, context):
-        zf = zipfile.ZipFile("/tmp/code/source")
-        try:
-            zf.extractall(path="/tmp/code")
-        except RuntimeError as e:
-            print(e)
+        with self.loadCodeLock:
+            r = requests.get(request.url)
+            try:
+                d = tempfile.mkdtemp('', '', '/tmp')
+                with open(d + "/func" , "wb") as code:
+                    code.write(r.content)
+                zf = zipfile.ZipFile(d + "/func")
+                zf.extractall(path=d)
+                zf.close()
+                sys.path.append(d)
+                if self.d is not None:
+                    sys.path.remove(self.d)
+                if self.func is None:
+                    self.func = importlib.import_module('index')
+                else:
+                    self.func = reload(self.func)
+                # TODO: unlink the last directory
+                self.d = d
+                self.funcName = request.funcName
+                return
+            except RuntimeError as e:
+                print(e)
 
     def Stop(self, request, context):
         return
@@ -33,7 +83,6 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
-
 
 if __name__ == '__main__':
     logging.basicConfig()
---
+++ src/tester.py
@@ -0,0 +1,18 @@
+import grpc
+import sys
+from container import container_pb2
+from container import container_pb2_grpc
+
+if __name__ == "__main__":
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = container_pb2_grpc.ContainerStub(channel)
+    res = stub.LoadCode(container_pb2.LoadCodeRequest(
+        funcName='test',
+        url=sys.argv[1],
+    ))
+    print(res)
+    res = stub.Invoke(container_pb2.InvokeRequest(
+        funcname='test',
+        payload=bytes('hello')
+    ))
+    print(res)
